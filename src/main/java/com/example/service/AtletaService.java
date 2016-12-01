1865ab98fa7ce94c72faea55776644d599a11bf5
dam <puchadesivan@gmail.com>
2016-12-01 20:58:36 +0100

    Empezando

--- src/main/java/com/example/service/AtletaService.java
+++ src/main/java/com/example/service/AtletaService.java
@@ -1,7 +1,24 @@
 package com.example.service;
 
+import com.example.domain.Atleta;
+import com.example.repository.AtletaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
 /**
  * Created by dam on 1/12/16.
  */
+@Service
 public class AtletaService {
+    @Autowired
+    private AtletaRepository atletaRepository;
+
+    public void testAtletas(){
+        Atleta atleta1 = new Atleta(1,"Ivan","Puchades","Española", LocalDate.of(1995,29,11));
+        AtletaRepository.save(atleta1);
+    }
+
+
 }
